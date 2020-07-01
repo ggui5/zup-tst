530cec19117beb6f7a182c39fe9bf983c063f45b
ggui5 <gabriel.ggui5@gmail.com>
2020-07-01 19:56:13 -0300

    init

---
+++ src/main/java/com/tst/zuptst/ZupTstApplication.java
@@ -0,0 +1,13 @@
+package com.tst.zuptst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupTstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupTstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tst/zuptst/ZupTstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tst.zuptst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupTstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
